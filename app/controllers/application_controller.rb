455fef30e7e425081f9d043d7f8987499dadaf69
kanta-sugawara <135586921+kanta-sugawara@users.noreply.github.com>
2023-07-17 01:21:49 +0000

    検索機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
+  
+  def set_search
+    @q = Review.ransack(params[:q])
+    @search_reviews = @q.result(distinct: true)
+  end
 
   def after_sign_in_path_for(resource)
     user_path(current_user)
